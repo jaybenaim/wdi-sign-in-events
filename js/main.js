94c4635f9e8c52f4300555a4818b56bbdbd153b2
jaybenaim <benaimjacob@gmail.com>
2019-08-20 15:32:13 -0400

    bonus complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => { 
+    let modal = $('.modal')
+    let m = document.querySelector('.modal')
 
     $('.signin').click(() => { 
-        // $('.modal').css('display', 'block') 
-        $('.modal').show() 
+        modal.show() 
     })
     $('.close').click(() => { 
-        // $('.modal').css('display', 'none')
-        $('.modal').hide() 
+        modal.hide() 
     }) 
+    $('.submit').click(() => { 
+        $('input').attr('class', 'error')
+    })
+    $('#user').mouseenter(function() { 
+        $(this).removeAttr('class', 'error')
+    })
+    
+    modal.click(function(e) { 
+        if (e.target === m) {
+            modal.hide() 
+        }
+
+    })
 })
+//  const modal = document.querySelector('.modal');
+//    modal.addEventListener('click', function (e) {
+//        if (e.target === modal) {
+//            modal.style.display = 'none'
+//        }
+//    })
