0dfe19e27e4265ab11af0c3fdc1e58ad7d162874
jaybenaim <benaimjacob@gmail.com>
2019-08-20 14:38:09 -0400

    changed method to show() and hide()

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => { 
 
     $('.signin').click(() => { 
-        $('.modal').css('display', 'block') 
+        // $('.modal').css('display', 'block') 
+        $('.modal').show() 
     })
     $('.close').click(() => { 
-        $('.modal').css('display', 'none')
+        // $('.modal').css('display', 'none')
+        $('.modal').hide() 
     }) 
 })
